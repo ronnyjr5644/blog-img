1f8c135dbddb972666af36e8f78640c7b0e5ec7c
crraj2 <crraj2@dovercorp.com>
2023-03-15 17:44:23 +0530

    first commit

--- public/js/s3Service.js
+++ public/js/s3Service.js
@@ -8,7 +8,7 @@ exports.s3Uploadv2 = async (files) => {
   const params = files.map((file) => {
     return {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `${uuid()}-${file.originalname}`,
+      Key: `${file.originalname}`,
       Body: file.buffer,
     };
   });
@@ -22,7 +22,7 @@ exports.s3Uploadv3 = async (files) => {
   const params = files.map((file) => {
     return {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `${uuid()}-${file.originalname}`,
+      Key: `${file.originalname}`,
       Body: file.buffer,
     };
   });
